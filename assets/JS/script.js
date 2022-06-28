886bd1855d8ebf75ff5f60690275c6aeda741c81
Marcos Vinicius <marcosvtenoriodev@gmail.com>
2022-06-28 09:16:50 -0300

    adicionando animação a pergunta e a resposta usando JS

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,40 +1,43 @@
 const inputPergunta = document.querySelector("#inputPergunta")
 const resposta = document.querySelector("#resposta")
 const listaRespostas = [
-    "Não tenho tanta certeza.",
-    "É decididamente assim.",
-    "Não conte com isso.",
-    "Sem dúvidas!",
-    "Pergunte novamente mais tarde.",
-    "Sim, definitivamente!",
-    "Minha resposta é não.",
-    "Você pode contar com isso.",
-    "Melhor não te dizer agora.",
-    "A meu ver, sim.",
-    "Minhas fontes dizem não.",
-    "Provavelmente.",
-    "Não é possível prever agora.",
-    "Perspectiva boa.",
-    "As perspectivas não são tão boas.",
-    "Sim.",
-    "Concentre-se e pergunte novamente.",
-    "Sinais apontam que sim.",
-    "Certeza!",
-]
-
+  "Não tenho tanta certeza.",
+  "É decididamente assim.",
+  "Não conte com isso.",
+  "Sem dúvidas!",
+  "Pergunte novamente mais tarde.",
+  "Sim, definitivamente!",
+  "Minha resposta é não.",
+  "Você pode contar com isso.",
+  "Melhor não te dizer agora.",
+  "A meu ver, sim.",
+  "Minhas fontes dizem não.",
+  "Provavelmente.",
+  "Não é possível prever agora.",
+  "Perspectiva boa.",
+  "As perspectivas não são tão boas.",
+  "Sim.",
+  "Concentre-se e pergunte novamente.",
+  "Sinais apontam que sim.",
+  "Certeza!", 
+] 
 
 //criar função para o botão fazer pergunta
-function fazerPergunta() {
+function fazerPergunta() { 
   
   if (inputPergunta.value == "") {
     alert("digite sua pergunta")
     return
   }
- 
-  //gerando um número inteiro aleatório da lista respostas
+
+  //gerando um número inteiro aleatório da lista respostas e transformando o número em uma frase
   const totalRespostas = listaRespostas.length
   const numeroAleatorio = Math.floor(Math.random() * totalRespostas)
-  resposta.innerHTML = (listaRespostas[numeroAleatorio])
+  const perguntaRepeat = "<div>" + inputPergunta.value + "</div>"
+  resposta.innerHTML = perguntaRepeat + (listaRespostas[numeroAleatorio])
   
-  
+  //animação de desaparecer as respostas
+  setTimeout(function(){resposta.style.opacity = "0"}, 6000);
+  setTimeout(function(){resposta.style.opacity = "1"}, 0);
+
 }
